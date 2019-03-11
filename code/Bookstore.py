0b022acaf4d5c5fb63a1aeeeb7280a1c061d43d4
Matteo Zignani <matteo.zignani@gmail.com>
2019-03-11 13:46:10 +0100

    Prima lezione

---
+++ code/Bookstore.py
@@ -0,0 +1,11 @@
+class Articolo(object):
+    def __init__():
+        
+    def __str__():
+        
+    def __eq__():
+        
+class Book(Articolo):
+    
+    def __init__():
+        super(Book,self).__init__():
